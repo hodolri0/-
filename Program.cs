1794a9ca6cf67df439ddbec50217afa14d51d6de
unknown <smhwang@lgcns.com>
2020-08-24 17:06:32 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace 김서방_찾기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] seoul = { "Jane", "Kim" };
+            
+            string result = Program.solution(seoul);
+
+            Console.WriteLine(result);
+        }
+        static public string solution(string[] seoul)
+        {
+            string answer = "";
+            List<string> s = seoul.ToList();
+            int c = 0;
+            for(int i= 0;i< s.Count; i++)
+            {
+                if(s[i] == "Kim")
+                {
+                    c = i;
+                    break;
+                }
+            }
+            answer = string.Format("김서방은 {0}에 있다", c.ToString());
+            return answer;
+        }
+    }
+}
